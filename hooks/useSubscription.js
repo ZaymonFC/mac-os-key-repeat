f4c57b7092401dcb31c3f9165781f99d756cb714
Zaymon Antonio <12402727+ZaymonFC@users.noreply.github.com>
2023-08-30 14:35:23 -0700

    Update useSubscription.js

--- hooks/useSubscription.js
+++ hooks/useSubscription.js
@@ -1,10 +1,28 @@
-import { useEffect } from "react";
+import React, { useEffect } from 'react';
 
-const useSubscription = (makeSubcription, deps) => {
-  useEffect(() => {
-    const sub = makeSubcription();
-    return () => sub?.unsubscribe();
-  }, deps);
+// Yoinked the name from the concept in swift.
+interface Cancellable {
+  unsubscribe: () => void;
+}
+
+/**
+ * Provides a wrapper around `React.useEffect` that manages the lifecycle
+ * of a **subscription**.
+ *
+ * The `subscription` will be **recreated** if any of the dependencies _change_.
+ *
+ * There is a custom rule in our `eslintrc.json` to enable the `exhaustive-deps`
+ * rule for this hook.
+ *
+ * @param subscribe - function that returns an object with an unsubscribe method.
+ * @param deps - dependencies for the subscription. If any of these change, the subscription will be recreated
+ */
+export const useSubscription = (
+  subscribe: () => Cancellable | void,
+  deps: React.DependencyList
+) => {
+  React.useEffect(() => {
+    const subscription = subscribe();
+    return () => subscription?.unsubscribe();
+  }, deps); // eslint-disable-line react-hooks/exhaustive-deps
 };
-
-export default useSubscription;
